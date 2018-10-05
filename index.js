173fef3645c6580097f81185f95ec5ffd733567c
Trevor Jameson <github email address>
2018-10-05 17:46:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // Should this mutate the var or return the result?
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "value";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new value";
+}
