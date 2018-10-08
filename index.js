7a110125176c26094543f37ffbf483a991f5513c
drubaby <peryton6@gmail.com>
2018-10-08 09:03:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'dru'
+
+function upperCaseCustomerName(name) {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(name) {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+// function leastFavoriteCustomer(){
+//   leastFavoriteCustomer = 'dru'
+// }
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'donnie'
+}
